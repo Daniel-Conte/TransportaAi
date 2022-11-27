5fe3ecaf5ba04dd2809a07a6427e46874de7b051
Daniel Conte <182736@upf.br>
2022-11-27 17:35:39 -0300

    altera titulo e pagina inicial

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>TransportaAi</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -46,9 +46,19 @@
 
             .title {
                 font-size: 84px;
-            }
+            } 
 
             .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .subtitle {
                 color: #636b6f;
                 padding: 0 25px;
                 font-size: 13px;
@@ -70,10 +80,10 @@
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
                     @else
-                        <a href="{{ route('login') }}">Login</a>
+                        <a href="{{ route('login') }}">Entrar</a>
 
                         @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
+                            <a href="{{ route('register') }}">Registre-se</a>
                         @endif
                     @endauth
                 </div>
@@ -81,18 +91,11 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    TransportaAi
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div class="subtitle"> 
+                    Gerencie seus transportes de carga de forma fácil e rápida!
                 </div>
             </div>
         </div>
